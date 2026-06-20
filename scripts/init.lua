7efae850aff66737796cf7ad3069d7198e458d46
AmondoVagyok <solokapitany10@gmail.com>
2026-06-20 12:53:36 +0200

    New planet icons + names

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,14 +1,56 @@
-ScriptHost:LoadScript("scripts/items_import.lua")
+ENABLE_DEBUG_LOG = true
+Tracker:AddItems("items/items.json")
 ScriptHost:LoadScript("scripts/utils.lua")
-ScriptHost:LoadScript("scripts/logic_helpers.lua")
 
 Tracker:AddMaps("maps/maps.json")
 
 -- Layout
-ScriptHost:LoadScript("scripts/layouts_import.lua")
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/tabs.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+Tracker:AddLayouts("layouts/planets.json")
+Tracker:AddLayouts("layouts/settings.json")
+Tracker:AddLayouts("layouts/settings_popup.json")
+Tracker:AddLayouts("layouts/weapons.json")
 
 -- Locations
 ScriptHost:LoadScript("scripts/locations_import.lua")
+
+print("------------------------DEBUG-----------------------")
+-- Table of All Planets
+Planets = {
+    "Oozla",
+    "Maktar",
+	"Endako",
+	"Barlow",
+    "Feltzin",
+    "Notak",
+    "Siberius",
+    "Tabora",
+    "Dobbo",
+    "Hrugis",
+    "Joba",
+    "Todano",
+    "Boldan",
+    "Aranos",
+    "Gorn",
+	"Snivelak",
+    "Smolg",
+    "Damosel",
+    "Grelbin",
+	"Yeedil"
+}
+print("Planet list: " .. Planets[1])
+for _, name in pairs(Planets) do
+---@type JsonItem
+---@diagnostic disable-next-line: assign-type-mismatch
+	local infobot = Tracker:FindObjectForCode(name)
+	print("Checking: " .. name)
+	infobot.BadgeText = string.sub(name, 1, 10)
+	infobot:SetOverlayFontSize(12)
+	infobot:SetOverlayAlign("center")
+end
 
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.18.0" then
